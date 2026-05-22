52e2f383c4874c2f72ec159242bf3d366dc8ffe1
Janithya Priyabhashana <janithya.pr@gmail.com>
2026-05-22 21:51:27 +0530

    created the main function

--- interview1/main.go
+++ interview1/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Count Vowels
+	total := CountVowels("Hello, world!")
+	fmt.Println("Total vowels:", total)
+}
+
+// Number of vowels in a given text
+func CountVowels(text string) int {
+	count := 0
+
+	for _, char := range strings.ToLower(text) {
+		switch char {
+		case 'a', 'e', 'i', 'o', 'u':
+			count++
+		}
+	}
+	return count
+}
