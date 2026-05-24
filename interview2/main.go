b1bb9e51df08a38a19ac4330dc2eaa72952311a0
Janithya Priyabhashana <janithya.pr@gmail.com>
2026-05-24 20:08:22 +0530

    checked how goroutine works

--- interview2/main.go
+++ interview2/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	// named function goroutine
+	go Task("A", &wg)
+	go Task("B", &wg)
+
+	// anonymous function goroutine
+	for i := 1; i <= 3; i++ {
+		go func(num int) {
+			fmt.Println("C", num)
+		}(i)
+	}
+	fmt.Println("Main function")
+
+	wg.Wait()
+}
+
+func Task(name string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i <= 5; i++ {
+		fmt.Println(name, i)
+		time.Sleep(2 * time.Second)
+	}
+}
